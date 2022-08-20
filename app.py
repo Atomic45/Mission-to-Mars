93686d48261f59a23e8781b41f455fd251a27f5a
Sandra G <sand2song@outlook.com>
2022-08-19 19:55:38 -0500

    adding app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for
-from flask_pymongo import PyMongo
+import PyMongo
 import scraping
 
 app = Flask(__name__)
@@ -18,9 +18,8 @@ def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
    mars.update_one({}, {"$set":mars_data}, upsert=True)
-   return redirect('/', code=302)
+   return "Scraping Successful"
 
-.update_one(query_parameter, {"$set": data}, options)
+   if __name__ == "__main__":
 
-if __name__ == "__main__":
-   app.run()
+      app.run(debug=True)
